e409b7306400928babf91cc5cd20794f191fc48c
drbaellow <duba1910@colorado.edu>
2024-02-06 20:32:43 -0500

    interview reflection

--- Vital Skills/5302 Cybersecurity/2.1.R
+++ Vital Skills/5302 Cybersecurity/2.1.R
@@ -1,6 +1,6 @@
 #1 A method for protecting information and communications through codes
 #2 cryptography related to cyber security by maintaining data integrity and ensuring data is only accessed by the appropriate users
-#3  social engeneering
+#3  social engineering
 #4 length, uppercase and lowercase, special characters, not re-using passwords
 #5 FALSE
 #6 the organization storing them
---
+++ Vital Skills/5302 Cybersecurity/3.1.R
@@ -0,0 +1,10 @@
+#1 Internet of things
+#2 the interconnectivity of all our smart devices 
+#3 phishing
+#4 it exploits human psychology to manipulate people into giving up sensitive info
+#5 false
+#6 helps an organization
+#7 unethical
+#8 between unethical and ethical
+#9 learn vulnerabilities
+#10 all 
