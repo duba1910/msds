eaf8c7890d26e6a55f766d517dce0d593e1aa342
drbaellow <duba1910@colorado.edu>
2024-02-20 15:29:10 -0500

    ethics final

---
+++ Vital Skills/5303 Ethics/3.1.R
@@ -0,0 +1,6 @@
+#1 a&B
+#2 all
+#3 tech
+#4 gender is unknown
+#5 top preformeer
+#6 a&c
---
+++ Vital Skills/5303 Ethics/4.1.R
@@ -0,0 +1,6 @@
+#1 A&B
+#2 A
+#3 certain words
+#4 histories of the neighborhoods
+#5 without consent
+#6 A&B
