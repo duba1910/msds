7d09a1fff97b06c5431f40f1ef62e4d11250d031
drbaellow <duba1910@colorado.edu>
2024-03-06 13:26:25 -0500

    Create 1.1.R

---
+++ Statistical Modeling/5011 Modern Regression Analysis/1.1.R
@@ -0,0 +1,10 @@
+
+#1 True
+#2 Individual Patient
+#3 Whether or not a patient will develop another tumor
+#4 Collection of all cancer patients
+#5 True
+#6 Socioeconomic status has been operationalized
+#7 should be carefully operationalized using relevant research
+#8 false
+
