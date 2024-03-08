1c9842c418ad296b5f25a828a6fa0cd1b091797b
drbaellow <duba1910@colorado.edu>
2024-03-07 19:34:24 -0500

    Create 1.2.R

---
+++ Statistical Modeling/5011 Modern Regression Analysis/1.2.R
@@ -0,0 +1,11 @@
+#1 FALSE
+#2 avoided
+#   && exploring and testing on same data
+#3 is the average change in sales for a one dollar increase in the Facebook advertising budget, assuming that the YouTube advertising budget is held constant.
+#4 the mean/expected sales for the sample average Facebook and YouTube advertising budgets.
+#5 Individual companies that sell the product whose sales are quantified in y
+#6 The set of all companies that sell the product whose sales are quantified in y
+#7 Each response measurement is assumed to have the same variance.
+#  && The existence of a linear relationship between the response variable and the model paramaters
+#  && Distinct random error terms must be independent
+#8 is the average change in sales for a one standard deviation increase in the YouTube advertising budget, assuming that the Facebook advertising budget is held constant.
